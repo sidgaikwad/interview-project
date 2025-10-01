baa166513265478f88c4a89071630b3283e23c81
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-10-01 12:07:41 +0530

    changes for the round 2

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,13 @@ type User = {
   picture: { large: string };
   location: { city: string; country: string };
   phone: string;
+  gender: string;
 };
 
 export default function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [filter, setFilter] = useState<string>("female");
+  const [searchItem, setSearchItem] = useState<string>("");
 
   useEffect(() => {
     fetch("https://randomuser.me/api?results=12")
@@ -19,10 +22,31 @@ export default function App() {
       .then((data) => setUsers(data.results));
   }, []);
 
+  const filteredUsers = users.filter((user) => {
+    const matchedGender = 
+    filter === "all" ? true : user.gender === filter;
+    // const fullName = `${user.name.first} ${user.name.last}`.toLowerCase();
+    // const 
+    return matchedGender;
+  })
+
   return (
     <main className="min-h-screen bg-gray-100 flex justify-center items-center p-8">
+      <div className="flelx flex-col sm:flex-row items-center gap-4 mb-8">
+        {/* {Gender filter code over here} */}
+        <div className="flex gap-2">
+          <button 
+          onClick={() => setFilter("all")}
+          className={`px-4 py-2 rounded-md ${
+            filter === "all" ? "bg-blue-500 text-white" : "bg-gray-300"}
+          `}
+          >
+            All
+          </button>
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
-        {users.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <div
             key={index}
             className="relative bg-white rounded-2xl shadow-md p-4 flex items-center border border-gray-200 transition-transform hover:scale-105 hover:shadow-xl"
